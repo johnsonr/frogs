228c7b1c3ffe8380d816398780af948b668674f2
Rod Johnson <johnsonroda@gmail.com>
2020-11-17 16:25:28 +1100

    Initial commit

---
+++ src/main/java/com/animals/aardvark/AardvarkApplication.java
@@ -0,0 +1,12 @@
+package com.animals.aardvark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AardvarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AardvarkApplication.class, args);
+	}
+}
